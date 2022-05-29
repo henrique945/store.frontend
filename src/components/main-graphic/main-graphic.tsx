bb37d07fea91e4c8c735ac522f086161b09a7006
Henrique <henrique.rodrigues7@hotmail.com>
2022-05-29 20:08:41 -0300

    create(table): add table info

--- src/components/main-graphic/main-graphic.tsx
+++ src/components/main-graphic/main-graphic.tsx
@@ -5,6 +5,7 @@ import { Bar, BarChart, Cell, Tooltip } from 'recharts';
 
 import { MainGraphicProxy } from '../../models/main-graphic.proxy';
 import Report from '../../services/report';
+import { getGraphicColor } from '../../utils/functions';
 import './main-graphic.css';
 
 //#endregion
@@ -43,11 +44,11 @@ function MainGraphic(): ReactElement {
         }
       </div>
 
-      {/* TODO: Adicionar informações no Tooltip */}
+      {/* TODO: Adicionar informações no Tooltip */ }
       <div className="graphic--data">
         <h6>Faturamento por ano</h6>
         <hr className="space"/>
-        <BarChart width={ 520 } height={ 150 } data={ chartData }>
+        <BarChart width={ 400 } height={ 150 } data={ chartData }>
           <Bar dataKey="value">
             {
               chartBarColors.map((entry, index) => (
@@ -63,43 +64,34 @@ function MainGraphic(): ReactElement {
   );
 }
 
-const getGraphicColor = ((year: number) => {
-  switch (year) {
-    case 2017:
-      return 'orange';
-    case 2018:
-      return 'green';
-    case 2019:
-      return 'red';
-    case 2020:
-      return 'blue';
-    case 2021:
-      return 'purple';
-  }
-});
-
-const chartBarColors = ['orange', 'green', 'red', 'blue', 'purple'];
+const chartBarColors = [
+  getGraphicColor(2017),
+  getGraphicColor(2018),
+  getGraphicColor(2019),
+  getGraphicColor(2020),
+  getGraphicColor(2021),
+];
 
 const chartData = [
   {
     name: '2017',
-    value: 4000,
+    value: 0,
   },
   {
     name: '2018',
-    value: 3000,
+    value: 0,
   },
   {
     name: '2019',
-    value: 3000,
+    value: 0,
   },
   {
     name: '2020',
-    value: 3000,
+    value: 0,
   },
   {
     name: '2021',
-    value: 3000,
+    value: 0,
   },
 ];
 
--- src/components/main-table/main-table.tsx
+++ src/components/main-table/main-table.tsx
@@ -1,13 +1,46 @@
 //#region Imports
 
-import { ReactElement } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
+import { MainTableProxy } from '../../models/main-table.proxy';
+import Report from '../../services/report';
+import TableRow from '../table-row/table-row';
 import './main-table.css';
 
 //#endregion
 
 function MainTable(): ReactElement {
+  const [tableInfo, setTableInfo] = useState<MainTableProxy[]>([]);
 
-  return (<h1>Main Table Component</h1>);
+  useEffect(() => {
+    const loadTableInfo = async () => {
+      const info = await Report.getTableInfo();
+
+      console.log(info);
+      setTableInfo(info);
+    };
+
+    loadTableInfo();
+  }, []);
+
+  return (
+    <div className="table">
+      <table>
+        <tr>
+          <th>Ano</th>
+          <th>Compras</th>
+          <th>Vendas</th>
+          <th>ICMS Pago</th>
+          <th>ST Pago</th>
+          <th></th>
+        </tr>
+        {
+          tableInfo.map((table) => (
+            <TableRow table={ table }/>
+          ))
+        }
+      </table>
+    </div>
+  );
 }
 
 export default MainTable;
